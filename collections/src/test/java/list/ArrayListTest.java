9139e00ac530e5597bcbd56ec0dd66dc3328a74e
piotrkrzyminski <piotrkrzyminski96@gmail.com>
2018-01-22 22:14:08 +0100

    Added new methods to ArrayList class, code refactoring
    Added new methods to ArrayList:
    	1. contains(E) - returns true if specified element is in list.
    	2. indexOf(E) - returns index of element from parameter.
    	3. replace(E,int) - change element on specified index.
    	4. trimToSize() - changes array size to fit all elements and save
    	memory usage
    	5. toArray() - converts list to array
    
    Changed remove method:
    	When removing element is in the beginning or in the middle. Moves
    	element after removed one to avoid null gaps in array.
    
    Added new tests for new and old methods.
    
    Code refactoring:
    	1. Avoided unneeded casts from in generic class.
    	2. Deleted unneded exceptions
    	3. Clean code

--- collections/src/test/java/list/ArrayListTest.java
+++ collections/src/test/java/list/ArrayListTest.java
@@ -53,6 +53,14 @@ public class ArrayListTest {
 	}
 	
 	@Test
+	public void testAddElementNotNull() throws IndexOutOfBoundsException, EmptyListException {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		assertTrue(list.add(Integer.valueOf(1)));
+		assertTrue(list.get(0) != null);
+	}
+	
+	@Test
 	public void testListResize() {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
@@ -78,14 +86,8 @@ public class ArrayListTest {
 		assertEquals(list.size(), 0);
 	}
 	
-	@Test(expected = EmptyListException.class)
-	public void testClearEmptyList() throws EmptyListException {
-		List<Integer> list = new ArrayList<Integer>(10);
-		list.clear();
-	}
-	
 	@Test
-	public void testGetElement() throws IllegalArgumentException, EmptyListException {
+	public void testGetElement() throws IllegalArgumentException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		for(int i = 0; i < 5; i++)
@@ -95,8 +97,8 @@ public class ArrayListTest {
 			assertTrue(list.get(i).equals(Integer.valueOf(i)));
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void testWrongElementIndex() throws IllegalArgumentException, EmptyListException {
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testWrongElementIndex() throws IndexOutOfBoundsException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		for(int i = 0; i < 5; i++)
@@ -105,40 +107,33 @@ public class ArrayListTest {
 		list.get(-1);
 	}
 	
-	@Test(expected = EmptyListException.class)
-	public void testGetElementFromEmptyList() throws IllegalArgumentException, EmptyListException {
-		List<Integer> list = new ArrayList<Integer>(10);
-		
-		list.get(0);
-	}
-	
 	@Test
-	public void testRemoveElement() throws IllegalArgumentException, EmptyListException {
+	public void testRemoveElement() throws IndexOutOfBoundsException, EmptyListException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		for(int i = 0; i < 5; i++)
 			list.add(Integer.valueOf(i));
 		
-		for(int i = 0; i < 5; i++)
-			assertTrue(list.remove(i).equals(Integer.valueOf(i)));
+		assertTrue(list.remove(3).equals(Integer.valueOf(3)));
+		assertEquals(list.get(3), Integer.valueOf(4));
 	}
 	
 	@Test (expected = EmptyListException.class)
-	public void testRemoveElementFromEmptyList() throws IllegalArgumentException, EmptyListException {
+	public void testRemoveElementFromEmptyList() throws IndexOutOfBoundsException, EmptyListException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		list.remove(0);
 	}
 	
-	@Test (expected = IllegalArgumentException.class)
-	public void testRemoveElementWrongIndex() throws IllegalArgumentException, EmptyListException {
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void testRemoveElementWrongIndex() throws IndexOutOfBoundsException, EmptyListException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		list.remove(-1);
 	}
 	
 	@Test
-	public void testListCloning() throws IllegalArgumentException, EmptyListException {
+	public void testListCloning() throws IndexOutOfBoundsException, EmptyListException {
 		List<Integer> list = new ArrayList<Integer>(10);
 		
 		for(int i = 0; i < 5; i++)
@@ -153,5 +148,84 @@ public class ArrayListTest {
 			assertTrue(list.get(i).equals(clonedList.get(i)));
 		}
 		
+	}
+	
+	@Test
+	public void testContainsElement() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		assertTrue(list.contains(Integer.valueOf(0)));
+		assertFalse(list.contains(Integer.valueOf(13424)));
+	}
+	
+	@Test
+	public void testIndexOfElement() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		assertEquals(list.indexOf(Integer.valueOf(2)), 2);
+		assertEquals(list.indexOf(Integer.valueOf(42)), -1);
+	}
+	
+	@Test
+	public void testIndexOfElementEmptyList() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		assertEquals(list.indexOf(Integer.valueOf(2)), -1);
+	}
+	
+	@Test
+	public void testReplacing() throws IndexOutOfBoundsException, EmptyListException {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		assertTrue(list.replace(Integer.valueOf(10), 3));
+		assertEquals(list.get(3), Integer.valueOf(10));
+	}
+	
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void testReplacingInvalidIndex() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		list.replace(Integer.valueOf(10), 13);
+		list.replace(Integer.valueOf(10), -1);
+	}
+	
+	@Test
+	public void testTrimToSize() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		list.trimToSize();
+		
+		assertEquals(list.size(), 5);
+		
+		for(int i = 0; i < 5; i++)
+			assertEquals(list.get(i), Integer.valueOf(i));
+	}
+	
+	@Test
+	public void testToArrayConversion() {
+		List<Integer> list = new ArrayList<Integer>(10);
+		
+		for(int i = 0; i < 5; i++)
+			list.add(Integer.valueOf(i));
+		
+		Object[] array = list.toArray();
+		
+		for(int i = 0; i < 5; i++)
+			assertEquals(list.get(i), array[i]);
 	}
 }
